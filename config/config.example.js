05f023bc39466fc9de4acf397e2d7ef393a5a49c
xutengfeng <xutengfeng@xutengfeng.lan>
2017-11-11 18:17:29 +0800

    remove config.js

---
+++ config/config.example.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  localApi: '*', // 本地api地址
+  lineApi: '*', // 云服务器上的api地址
+}
---
+++ config/config.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  localApi: 'http://localhost:2366',
+  lineApi: 'http://112.74.40.54:2366',
+}
