b922d9466a329abb3b402bb8da1013504dbb8ba2
xutengfeng <xutengfeng@xutengfeng.lan>
2017-11-11 18:28:52 +0800

    fix config

--- config/config.example.js
+++ config/config.example.js
@@ -1,6 +1,15 @@
 'use strict';
+const prod = true;
+const local = false;
 
-module.exports = {
-  localApi: '*', // 本地api地址
-  lineApi: '*', // 云服务器上的api地址
+if (prod) {
+  console.log('running in aliyun');
+  module.exports = {
+    backend: '*' // 云服务器上的api地址
+  }
+} else {
+  console.log('running in local');
+  module.exports = {
+    backend: '*', // 本地api地址
+  }
 }
